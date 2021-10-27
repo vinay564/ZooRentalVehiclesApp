4b9f6e574cadf71e7ae9150c0db4cf72be8ec012
vinay564 <38555014+vinay564@users.noreply.github.com>
2021-10-27 12:52:19 -0500

    Initial commmit(Added Spring Dependencies)

---
+++ src/main/java/edu/nwmissouri/teambearcats/ZooRentalVehiclesAppApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.teambearcats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooRentalVehiclesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooRentalVehiclesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/teambearcats/ZooRentalVehiclesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.teambearcats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooRentalVehiclesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
