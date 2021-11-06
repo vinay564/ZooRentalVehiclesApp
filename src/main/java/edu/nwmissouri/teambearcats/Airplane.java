2d55ad8eedd3ab2204c3777a30635b79c18e44dd
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-05 23:13:37 -0500

    added javadocs

--- src/main/java/edu/nwmissouri/teambearcats/Airplane.java
+++ src/main/java/edu/nwmissouri/teambearcats/Airplane.java
@@ -5,63 +5,101 @@
 package edu.nwmissouri.teambearcats;
 
 /**
- *
+ *Airplane class (derived subclass of the superclass RentalHeavyVehicles)
  * @author Satish Vagulapuram
  */
 public class Airplane extends RentalHeavyVehicles {
 
+/**
+ * Declaration of private variables
+ */
     private int distanceTravelled = 4;
     private double ticketPrice = 0.0;
     private int noOfWings = 2;
-
+/**
+ * getter method for DistanceTravelled
+ * @return 
+ */
     public int getDistanceTravelled() {
         return distanceTravelled;
     }
-
+/**
+ * setter method for DistanceTravelled
+ * @return 
+ */
     public void setDistanceTravelled(int distanceTravelled) {
         this.distanceTravelled = distanceTravelled;
     }
-
+/**
+ * getter method for ticketPrice
+ * @return 
+ */
     public double getTicketPrice() {
         return ticketPrice;
     }
-
+/**
+ * setter method
+ * @param ticketPrice 
+ */
     public void setTicketPrice(double ticketPrice) {
         this.ticketPrice = ticketPrice;
     }
-
+/**
+ * getter method for getOfWings
+ * @return 
+ */
     public int getNoOfWings() {
         return noOfWings;
     }
+/**
+ * setter method
+ * @param noOfWings 
+ */
 
     public void setNoOfWings(int noOfWings) {
         this.noOfWings = noOfWings;
     }
-
+/**
+ * toString method
+ * @return 
+ */
     @Override
     public String toString() {
         return "Airplane{" + "distanceTravelled=" + distanceTravelled + ", ticketPrice=" + ticketPrice + ", noOfWings=" + noOfWings + '}';
     }
-
+/**
+ * Constructor
+ * @param vehicleName
+ * @param carryHeavyLoad
+ * @param vehicleWeight 
+ */
     public Airplane(String vehicleName, String carryHeavyLoad, double vehicleWeight) {
         super(vehicleName, carryHeavyLoad, vehicleWeight);
     }
-
+/**
+ * Overriding the move method 
+ */
     @Override
     public void move() {
         System.out.println("I move on air");
     }
-
+/**
+ * Overriding the capacity method
+ */
     @Override
     public void capacity() {
         System.out.println("I have more passenger capacity");
     }
-
+/**
+ * Overriding the price
+ */
     @Override
     public void price() {
         System.out.println("I'm costly");
     }
-
+/**
+ * Overriding the foodMenu
+ */
     public void foodMenu() {
         System.out.println("I provide food while travelling");
     }
--- src/main/java/edu/nwmissouri/teambearcats/Helicopter.java
+++ src/main/java/edu/nwmissouri/teambearcats/Helicopter.java
@@ -5,7 +5,7 @@
 package edu.nwmissouri.teambearcats;
 
 /**
- *
+ *Helicopter class (derived subclass of the superclass RentalHeavyVehicles)
  * @author Satish Vagulapuram
  */
 public class Helicopter extends RentalHeavyVehicles {
@@ -13,58 +13,95 @@ public class Helicopter extends RentalHeavyVehicles {
     private int numOfBlades = 4;
     private String nameOfPillots = "jerry";
     private int distanceTravelled = 5;
-
+/**
+ * getter method for getNumOfBlades
+ * @return 
+ */
     public int getNumOfBlades() {
         return numOfBlades;
     }
-
+/**
+ * setter method
+ * @param numOfBlades 
+ */
     public void setNumOfBlades(int numOfBlades) {
         this.numOfBlades = numOfBlades;
     }
-
+/**
+ * getter method for getNameOfPillots
+ * @return 
+ */
     public String getNameOfPillots() {
         return nameOfPillots;
     }
-
+/**
+ * setter method
+ * @param nameOfPillots 
+ */
     public void setNameOfPillots(String nameOfPillots) {
         this.nameOfPillots = nameOfPillots;
     }
-
+/**
+ * getter method for getDistanceTravelled
+ * @return 
+ */
     public int getDistanceTravelled() {
         return distanceTravelled;
     }
-
+/**
+ * setter method
+ * @param distanceTravelled 
+ */
     public void setDistanceTravelled(int distanceTravelled) {
         this.distanceTravelled = distanceTravelled;
     }
-
+/**
+ * ToString 
+ * @return 
+ */
     @Override
     public String toString() {
         return "Helicopter{" + "numOfBlades=" + numOfBlades + ", nameOfPillots=" + nameOfPillots + ", distanceTravelled=" + distanceTravelled + '}';
     }
-
+/**
+ * Constructor
+ * @param numOfBlades
+ * @param nameOfPillots
+ * @param distanceTravelled
+ * @param vehicleName
+ * @param carryHeavyLoad
+ * @param vehicleWeight 
+ */
     public Helicopter(int numOfBlades, String nameOfPillots, int distanceTravelled, String vehicleName, String carryHeavyLoad, double vehicleWeight) {
         super(vehicleName, carryHeavyLoad, vehicleWeight);
         this.numOfBlades = numOfBlades;
         this.nameOfPillots = nameOfPillots;
         this.distanceTravelled = distanceTravelled;
     }
-
+/**
+ * Overriding the move method
+ */
     @Override
     public void move() {
         System.out.println("I move on air less than airplane height");
     }
-
+/**
+ * Overriding the capacity method
+ */
     @Override
     public void capacity() {
         System.out.println("I have less capacity");
     }
-
+/**
+ * Overriding the price
+ */
     @Override
     public void price() {
         System.out.println("I'm cheaper than plane");
     }
-
+/**
+ * duration method
+ */
     public void duration() {
 
         System.out.println("My duration varies on cost");
