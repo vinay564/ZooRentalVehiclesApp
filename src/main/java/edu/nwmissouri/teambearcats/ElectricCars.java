d5adc51cfee02c688eac16c8ff3b0eff2d6a012a
Nikhil Porika <nikhil.porika36@gmail.com>
2021-11-05 23:09:21 -0500

    Updated the javadocs in the ElectricCars and FuelCars classes

--- src/main/java/edu/nwmissouri/teambearcats/ElectricCars.java
+++ src/main/java/edu/nwmissouri/teambearcats/ElectricCars.java
@@ -5,14 +5,15 @@
 package edu.nwmissouri.teambearcats;
 
 /**
- *Electric class (derived subclass of the superclass RentalLightvehicles)
- * 
+ * Electric class (derived subclass of the superclass RentalLightvehicles)
+ *
  * @author Nikhil Porika
  */
 public class ElectricCars extends RentalLightVehicles {
+
     private int numOfWheels = 4;
-    private double price=0.0;
-    private double mile=0.0;
+    private double price_dollar = 25.70;
+    private double mile = 15.40;
 
     public int getNumOfWheels() {
         return numOfWheels;
@@ -23,11 +24,11 @@ public class ElectricCars extends RentalLightVehicles {
     }
 
     public double getPrice() {
-        return price;
+        return price_dollar;
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        this.price_dollar = price;
     }
 
     public double getMile() {
@@ -38,31 +39,40 @@ public class ElectricCars extends RentalLightVehicles {
         this.mile = mile;
     }
 
+    /*
+    toString method for the fuelcars vehicle class
+     */
     @Override
     public String toString() {
-        return "ElectricCars{" + "numOfWheels=" + numOfWheels + ", price=" + price + ", mile=" + mile + '}';
+        return "ElectricCars{" + "numOfWheels=" + numOfWheels + ", price=" + price_dollar + ", mile=" + mile + '}';
     }
 
     /**
      * Electric Cars constructor
+     *
      * @param vehicleName - The Name of the vehicle
-     * @param booking -  The Different booking systems
+     * @param booking - The Different booking systems
      * @param numOfPassengers - Number of Passengers who travel
      * @param numOfMiles - Number of Miles car travels
      */
     public ElectricCars(String vehicleName, String booking, int numOfPassengers, Double numOfMiles) {
         super(vehicleName, booking, numOfPassengers, numOfMiles);
     }
-   
+
+    /*
+    move method definest the vehicle move type
+     */
     @Override
     public void move() {
         System.out.println("The Electric car which is available in the Zoo can move in differe climatic conditions of zoo");
     }
 
+    /*
+    speed method defines the vehicle speed type
+     */
     @Override
     public void speed() {
         System.out.println("In the Zoo the electric car can travel with a certain speed which is for the suitable to the zoo situations");
     }
-    
-    
-    }
+
+}
