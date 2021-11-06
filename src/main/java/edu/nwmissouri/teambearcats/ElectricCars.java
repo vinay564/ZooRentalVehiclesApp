a9769bfddee30378712c1a1c6f931eb03564d633
Nikhil Porika <nikhil.porika36@gmail.com>
2021-11-05 21:09:53 -0500

    Added the new changed code in the ElectricCars and FuelCars in the ZooRentalVehiclesApp

--- src/main/java/edu/nwmissouri/teambearcats/ElectricCars.java
+++ src/main/java/edu/nwmissouri/teambearcats/ElectricCars.java
@@ -5,9 +5,64 @@
 package edu.nwmissouri.teambearcats;
 
 /**
- *
+ *Electric class (derived subclass of the superclass RentalLightvehicles)
+ * 
  * @author Nikhil Porika
  */
-public class ElectricCars {
+public class ElectricCars extends RentalLightVehicles {
+    private int numOfWheels = 4;
+    private double price=0.0;
+    private double mile=0.0;
+
+    public int getNumOfWheels() {
+        return numOfWheels;
+    }
+
+    public void setNumOfWheels(int numOfWheels) {
+        this.numOfWheels = numOfWheels;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMile() {
+        return mile;
+    }
+
+    public void setMile(double mile) {
+        this.mile = mile;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCars{" + "numOfWheels=" + numOfWheels + ", price=" + price + ", mile=" + mile + '}';
+    }
+
+    /**
+     * Electric Cars constructor
+     * @param vehicleName - The Name of the vehicle
+     * @param booking -  The Different booking systems
+     * @param numOfPassengers - Number of Passengers who travel
+     * @param numOfMiles - Number of Miles car travels
+     */
+    public ElectricCars(String vehicleName, String booking, int numOfPassengers, Double numOfMiles) {
+        super(vehicleName, booking, numOfPassengers, numOfMiles);
+    }
+   
+    @Override
+    public void move() {
+        System.out.println("The Electric car which is available in the Zoo can move in differe climatic conditions of zoo");
+    }
+
+    @Override
+    public void speed() {
+        System.out.println("In the Zoo the electric car can travel with a certain speed which is for the suitable to the zoo situations");
+    }
     
-}
+    
+    }
--- src/main/java/edu/nwmissouri/teambearcats/FuelCars.java
+++ src/main/java/edu/nwmissouri/teambearcats/FuelCars.java
@@ -8,6 +8,62 @@ package edu.nwmissouri.teambearcats;
  *
  * @author Nikhil Porika
  */
-public class FuelCars {
+public class FuelCars extends RentalLightVehicles {
+    private int numOfWheels = 4;
+    private double price = 0.0;
+    private boolean IsHavingAirBags = true;
+
+    public int getNumOfWheels() {
+        return numOfWheels;
+    }
+
+    public void setNumOfWheels(int numOfWheels) {
+        this.numOfWheels = numOfWheels;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isIsHavingAirBags() {
+        return IsHavingAirBags;
+    }
+
+    public void setIsHavingAirBags(boolean IsHavingAirBags) {
+        this.IsHavingAirBags = IsHavingAirBags;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelCars{" + "numOfWheels=" + numOfWheels + ", price=" + price + ", IsHavingAirBags=" + IsHavingAirBags + '}';
+    }
+     
+    /**
+     * FuelCars Constructor
+     * @param vehicleName - This the name of the vehicle 
+     * @param booking - Need to book a fuel cars
+     * @param numOfPassengers - Passengers number in the car
+     * @param numOfMiles - Miles of the car
+     */ 
+    
+    public FuelCars(String vehicleName, String booking, int numOfPassengers, Double numOfMiles) {
+        super(vehicleName, booking, numOfPassengers, numOfMiles);
+    }
+
+     
+    
+    @Override
+    public void move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void speed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
