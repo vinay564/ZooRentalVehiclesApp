83bc38f8612c68a16ffc0435e542bb70e603b893
VagulapuramSatish <satish.vagulapuram@gmail.com>
2021-11-09 19:17:42 -0600

    added unit tests

---
+++ src/test/java/edu/nwmissouri/teambearcats/AirplaneTest.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.nwmissouri.teambearcats;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *Test Airplane using JUnit5
+ * @author Satish Vagulapuram
+ */
+public class AirplaneTest {
+    
+    public AirplaneTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDistanceTravelled method, of class Airplane.
+     */
+    @Test
+    public void testGetDistanceTravelled() {
+    }
+
+    /**
+     * Test of setDistanceTravelled method, of class Airplane.
+     */
+    @Test
+    public void testSetDistanceTravelled() {
+    }
+
+    /**
+     * Test of getTicketPrice method, of class Airplane.
+     */
+    @Test
+    public void testGetTicketPrice() {
+    }
+
+    /**
+     * Test of setTicketPrice method, of class Airplane.
+     */
+    @Test
+    public void testSetTicketPrice() {
+    }
+
+    /**
+     * Test of getNoOfWings method, of class Airplane.
+     */
+    @Test
+    public void testGetNoOfWings() {
+    }
+
+    /**
+     * Test of setNoOfWings method, of class Airplane.
+     */
+    @Test
+    public void testSetNoOfWings() {
+    }
+
+    /**
+     * Test of toString method, of class Airplane.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    /**
+     * Test of move method, of class Airplane.
+     */
+    @Test
+    public void testMove() {
+    }
+
+    /**
+     * Test of capacity method, of class Airplane.
+     */
+    @Test
+    public void testCapacity() {
+    }
+
+    /**
+     * Test of price method, of class Airplane.
+     */
+    @Test
+    public void testPrice() {
+    }
+
+    /**
+     * Test of foodMenu method, of class Airplane.
+     */
+    @Test
+    public void testFoodMenu() {
+    }
+    
+}
